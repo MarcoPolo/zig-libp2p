fd19fb5ee5763a8c277b2eaa6be4f27e77d3e098
Marco Munizaga <git@marcopolo.io>
2022-05-12 16:04:27 -0700

    mbedtls playground

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+const mbedtls = @import("zig-mbedtls/mbedtls.zig");
 const pkgs = struct {
     const network = std.build.Pkg{
         .name = "network",
@@ -12,14 +13,20 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    const target = b.standardTargetOptions(.{});
+
+    const mbedtls_lib = mbedtls.create(b, target, mode);
+
     const lib = b.addStaticLibrary("zig-libp2p", "src/main.zig");
     lib.setBuildMode(mode);
     lib.addPackage(pkgs.network);
+    mbedtls_lib.link(lib);
     lib.install();
 
     const main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
     main_tests.addPackage(pkgs.network);
+    mbedtls_lib.link(main_tests);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
+const mbedtls = @cImport({
+    @cInclude("mbedtls/net_sockets.h");
+    @cInclude("mbedtls/debug.h");
+    @cInclude("mbedtls/ssl.h");
+    @cInclude("mbedtls/entropy.h");
+    @cInclude("mbedtls/ctr_drbg.h");
+    @cInclude("mbedtls/error.h");
+    @cInclude("mbedtls/certs.h");
+    @cInclude("mbedtls/timing.h");
+
+    @cInclude("mbedtls/platform.h");
+});
 const testing = std.testing;
+
+// What needs to be built
+//
+// PoC ping protocol.
+// 1. Peer Identity
+// 2. TCP connection
+// 3. TLS upgrade
+// 4. Yamux stream
+// 5. quic ? Maybe replace 2->4
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
@@ -7,4 +28,12 @@ export fn add(a: i32, b: i32) i32 {
 
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
+}
+
+test "basic mbedtls test" {
+    var foo: mbedtls.mbedtls_ssl_context = undefined;
+    mbedtls.mbedtls_ssl_init(&foo);
+    _ = foo;
+
+    try testing.expect(true);
 }
